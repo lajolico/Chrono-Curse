0ece806b36bd0ad05237af7cdffab20279a5b85b
Connor Contursi <77695646+cwcontur@users.noreply.github.com>
2023-04-20 11:07:51 -0400

    updatingfixes

---
+++ Chrono Curse/Assets/Enemies/PigSmall.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class PigSmall : MonoBehaviour
+{
+    public int maxHealth = 100;
+    int currentHealth;
+
+    public GameObject smallPig;
+    public GameObject enemyAI;
+
+    private bool isInRange = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            // Debug.Log("Attack!!!");
+            enemyAI.GetComponent<EnemyAI>().AttackingPlayer(isInRange);
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = false;
+            // Debug.Log("Not attacking...");
+            enemyAI.GetComponent<EnemyAI>().AttackingPlayer(isInRange);
+        }
+    }
+
+    void Die()
+    {
+        Destroy(smallPig);
+        smallPig.transform.parent.gameObject.SetActive(false);
+    }
+}
